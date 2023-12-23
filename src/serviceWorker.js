26ffef56749873c5e2b2f6cddac8ad4f897a520c
developer-tvc <developer@techversantinfo.com>
2023-12-23 17:42:02 +0530

    fix

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -7,14 +7,14 @@ const urlsToCache = [
   '/logo512.png',
 ];
 
-self.addEventListener('install', event => {
+window.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+window.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
